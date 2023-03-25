698e1d749942ebf2eaf3544f2a42b059c89e7572
mosespace <kisakyemoses50@gmail.com>
2023-03-25 13:48:36 +0300

    feat: added comments

--- send.php
+++ send.php
@@ -18,7 +18,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the 'input-box' field is empty
     if (empty($name)) {
         // If it's empty, display an alert message using JavaScript
-        echo '<script>alert("Field can not be empty!")</script>';
+        echo "<script>
+        alert('Can Not Add An Empty List!')
+        </script>";
     } else {
         // If it's not empty, add its value to the 'items' array in the session
         $_SESSION['items'][] = $name;
@@ -38,6 +40,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <div class="container">
+
+    <!-- Begining Of Form -------------------------------------------------------------------------! -->
     <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
             <div class="todo-app">
                 <h2>To DO | List<img src="./images/icon.png" alt="This is the logo"></h2>
@@ -45,6 +49,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <input type="text" name="input-box" placeholder="Add your text">
                             <button onclick="addTask()">Add</button>
                     </div>
+
+                    <!-- Here i created the Un-Orderd List! -->
                     <ul id="list-container">
 
 
@@ -63,7 +69,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 ?>
 
 
-                // Inline css style! 
+                <!-- Here I decided to do an Inline css style!  -->
                 <style>
                         ul li span.close {
                         position: absolute !important;
@@ -80,83 +86,64 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     ul li span.close:hover {
                         background: #edeef0;
                     }
-                </style>
-
+                </style> <!-- End of css-------------------------------------------------------------------------! -->
+                
+            
                     </ul>
-                    
+<!-- End of Un-Orderd-List--------------------------------------------------------------------------------------------------!  -->
             </div>
-    </form>
+    </form> <!-- End of Form-------------------------------------------------------------------------!          -->
     </div>
+
+    <!-- Begin of JS Script -->
      <script>
-        // Get all the list items
+        // Here is select all list items in the list container
         var listItems = document.querySelectorAll("#list-container li");
 
-        // Add an event listener to each list item
+        // Here i add a click event listener to each list item
         listItems.forEach(function(listItem) {
             listItem.addEventListener("click", function() {
-                // Toggle the 'checked' class on the list item
+                // Then here i toggled the 'checked' class on the clicked list item
                 this.classList.toggle("checked");
             });
         });
 
+        // And then i selected all elements with the class 'close'
         var closeSpan = document.querySelectorAll('.close');
 
-// Loop through each span and add a click event listener
-function removeTask(index) {
-  var listItem = document.getElementById("item-" + index);
-  
-  // Check if the list item has the 'checked' class
-  if (listItem.classList.contains('checked')) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "delete.php");
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        // Remove the li element from the list
-        listItem.parentNode.removeChild(listItem);
-      } else {
-        alert('An error occurred while deleting the item.');
-      }
-    };
-    xhr.send("index=" + index);
-  } else {
-    alert('Item must be checked before it can be removed.');
-  }
-}
+        // I added a JS Function to remove a task
+        function removeTask(index) {
+        // Find the list item with the given index
+        var listItem = document.getElementById("item-" + index);
+        
+        // Here am trying to check if the list item has the 'checked' class
+        if (listItem.classList.contains('checked')) {
+            // I Created a new XMLHttpRequest object
+            var xhr = new XMLHttpRequest();
+            // I set the HTTP method and URL for the request
+            xhr.open("POST", "delete.php");
+            // I set the request header
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            // Here i define what to do when the response is received
+            xhr.onload = function() {
+            if (xhr.status === 200) {
+                // If the response is successful, remove the list item from the DOM
+                listItem.parentNode.removeChild(listItem);
+            } else {
+                // If there is an error, display an alert message
+                alert('An error occurred while deleting the item.');
+            }
+            };
+            // He i finally send the request with the index of the task to be removed
+            xhr.send("index=" + index);
+        } else {
+            // If the list item is not checked, display an alert message
+            alert('Item must be checked before it can be removed.');
+        }
+        }
+            // In summary, this code adds a click event listener to each list item in a container element. When a list item is clicked, it toggles the 'checked' class on that list item. Additionally, there is a function that can be called to remove a task from the list. This function sends a POST request to a PHP file with the index of the task to be removed. If the task is checked, the function removes the corresponding list item from the DOM. Otherwise, it displays an alert message.
 
-
-
-
-//    // Get all the close spans
-// var closeSpan = document.querySelectorAll('.close');
-
-// // Loop through each span and add a click event listener
-// closeSpan.forEach(function(span) {
-//     span.addEventListener('click', function() {
-//         // Get the parent li element of the span
-//         var listItem = span.parentNode;
-
-//         // Get the index of the item being deleted
-//         var index = Array.prototype.indexOf.call(listItem.parentNode.children, listItem);
-
-//         // Remove the item from the session array
-//         if (index > -1) {
-        //   { <?php unset($_SESSION['items']['']); ?>;}
-//         }
-
-//         // Remove the li element from the list
-//         listItem.parentNode.removeChild(listItem);
-//     });
-// });
-// listContainer.addEventListener("click", function(e){
-//     if(e.target.tagName === "LI"){
-//         e.target.classList.toggle("listItems");
-//     }
-//     else if(e.target.tagName === "SPAN"){
-//         e.tagName.parent.Element.remove();
-//     }
-// }, false);
-    
     </script>
+    <!-- End Of Js Script -->
 </body>
 </html>
