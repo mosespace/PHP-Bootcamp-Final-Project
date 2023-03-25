d84239e7b360897898a285d786075e3a6ba78db7
mosespace <kisakyemoses50@gmail.com>
2023-03-25 13:04:12 +0300

    feat: added comments

--- delete.php
+++ delete.php
@@ -1,3 +1,4 @@
+<!-- This file  handles the deletion of the item from the $_SESSION['items'] array. -->
 <?php
 // start the session
 session_start();
@@ -11,17 +12,6 @@ if (isset($_POST['index'])) {
   if (isset($_SESSION['items'][$index])) {
     // remove the item from the items array in the session
     unset($_SESSION['items'][$index]);
-    // send a success response
-    http_response_code(200);
-    echo "Item deleted successfully.";
-  } else {
-    // send an error response
-    http_response_code(400);
-    echo "Invalid index parameter.";
   }
-} else {
-  // send an error response
-  http_response_code(400);
-  echo "Index parameter not set.";
 }
 ?>
